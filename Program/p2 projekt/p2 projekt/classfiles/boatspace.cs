9f4a2d500f9819516072124e4c48ca118b78d637
Abstrakten <kterndrupxd@gmail.com>
2014-03-11 14:13:17 +0100

    hello.. is it me you're looking for?!

---
+++ Program/p2 projekt/p2 projekt/classfiles/boatspace.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p2_projekt.classfiles
+{
+    abstract class boatspace
+    {
+    }
+
+    class waterspace : boatspace
+    {
+    }
+
+    class landspace : boatspace
+    {
+    }
+}
