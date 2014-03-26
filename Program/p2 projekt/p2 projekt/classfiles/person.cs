8f301babc13024863383951fa684183b9b0c4247
Zander <moonfrogdk@gmail.com>
2014-03-26 16:09:45 +0100

    dfgsfdgsfhsgh

--- Program/p2 projekt/p2 projekt/classfiles/person.cs
+++ Program/p2 projekt/p2 projekt/classfiles/person.cs
@@ -7,17 +7,22 @@ using System.Device.Location;
 
 namespace p2_projekt.classfiles
 {
+    class Permissions{
+        bool canReadEvent = false;
+        // add more...
+    }
     abstract class Person
     {
-        public abstract int Id { get; set; }
+        private int Id { get; set; }// Unique. Only used internally
                 
-        public List<Boat> boats;
+        public List<Boat> boats; // boats owned
 
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
         public CivicAddress Adress { get; set; }
-        public bool Disabled { get; set; }
+        public bool IsActive { get; set; } // Still active in club
 
+        public Permissions Permissions; // must be initialized with no access at all. E.g = new Permissions().LowestAccess;
 
         public Person(string name)
         {
@@ -28,19 +33,25 @@ namespace p2_projekt.classfiles
     
     class Member : Person
     {
-        public int Id { get; set; }
 
-        public int Membership { get; set; }
-              
+        public int MembershipNumber { get; private set; } // backwards compatible with existing numbers from Vestre Baadelaug database.
+
+        public List<Travel> Travels { get; set; } // All travels. Old and new.
+        
         public Member(string name, CivicAddress adress) : base(name)
         {
-            
+            // assign membership number
         }
+    }
+    
+    // has no member ID
+    class HarbourMaster : Person
+    {
+
     }
 
     class Guest : Person
     {
-        public int Id { get; set; }
 
     }
 }
