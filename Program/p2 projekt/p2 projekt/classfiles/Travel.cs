a47a2f931afa82da103c762a1f0685e15ae8d44f
Zander <moonfrogdk@gmail.com>
2014-03-27 08:44:27 +0100

    travel

---
+++ Program/p2 projekt/p2 projekt/classfiles/Travel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace p2_projekt.classfiles
+{
+    class Travel
+    {
+        public DateTime Start { get; set; } 
+        public DateTime End { get; set; }
+
+        public bool isActive { get; }// must change based on today
+    }
+}
