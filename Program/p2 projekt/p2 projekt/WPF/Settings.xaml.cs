084d93670e8751889bdb6cf3b1de2e724fe1c3bf
Zander <moonfrogdk@gmail.com>
2014-04-02 08:50:08 +0200

    settings

---
+++ Program/p2 projekt/p2 projekt/WPF/Settings.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+
+namespace p2_projekt.WPF
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    [ContentProperty("Children")]
+    public partial class Settings : ItemsControl
+    {
+        UIElementCollection Col;
+
+        bool isreadonly;
+
+        public Boolean ReadOnly { get { return isreadonly; } 
+            set { 
+                isreadonly = value;
+                foreach(InfolineController info in Col)
+                {
+                    info.readOnly = value;
+                }
+            }}
+        public UIElementCollection Children { get { return Col; } set { Col = value; } }
+        public Settings()
+        {
+            InitializeComponent();
+            Children = stack.Children;
+        }
+    }
+}
