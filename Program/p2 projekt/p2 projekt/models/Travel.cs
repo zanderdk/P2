bce1eabefc6124998c6b7b749197dabf6ae56ba9
Abstrakten <kterndrupxd@gmail.com>
2014-04-15 10:53:03 +0200

    trvls

--- Program/p2 projekt/p2 projekt/models/Travel.cs
+++ Program/p2 projekt/p2 projekt/models/Travel.cs
@@ -20,7 +20,7 @@ namespace p2_projekt.models
             }
         }
 
-        public virtual User User { get; set; }
+        public User User { get; set; }
 
         public Travel(DateTime start, DateTime end)
         {
