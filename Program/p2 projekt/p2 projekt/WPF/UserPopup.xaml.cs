df054b2156ff4d59ec25baf1095ba72903764a58
Clegood <Mikkel-m@hotmail.com>
2014-05-02 13:46:50 +0200

    Lobop Design V2

---
+++ Program/p2 projekt/p2 projekt/WPF/UserPopup.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace p2_projekt.WPF
+{
+    /// <summary>
+    /// Interaction logic for UserPopup.xaml
+    /// </summary>
+    public partial class UserPopup : Window
+    {
+        public UserPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
