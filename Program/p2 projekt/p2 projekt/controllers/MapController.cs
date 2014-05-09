270b952685e4193bf7fbce18436591afe797a220
Simon Vandel Sillesen <simon.vandel@gmail.com>
2014-05-09 10:57:04 +0200

    flyt tabmap

--- Program/p2 projekt/p2 projekt/controllers/MapController.cs
+++ Program/p2 projekt/p2 projekt/controllers/MapController.cs
@@ -10,5 +10,20 @@ namespace p2_projekt.controllers
 {
     class MapController
     {
+        public void FillBoatSpaceRepresentations(List<BoatSpaceRepresentation> boatSpaceRepresentations)
+        {
+            DALController us = Utilities.LobopDB;
+
+            var boatSpaces = us.ReadAll<BoatSpace>(x => x != null).ToList();
+
+
+            for (int i = 0; i < boatSpaces.Count; i++)
+            {
+                BoatSpace bs = boatSpaces[i];
+                BoatSpaceRepresentation bsr = boatSpaceRepresentations[i];
+                bsr.BoatSpace = bs;
+                bsr.Label.Content = bs.BoatSpaceId;
+            }
+        }
     }
 }
