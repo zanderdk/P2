5a216dde2103ef47a097269504ff1dbb0380e23c
Clegood <Mikkel-m@hotmail.com>
2014-05-12 12:59:04 +0200

    --II--

--- Program/p2 projekt/p2 projekt/DatabaseCreator.cs
+++ Program/p2 projekt/p2 projekt/DatabaseCreator.cs
@@ -18,7 +18,7 @@ namespace p2_projekt
         readonly string[] _email = { "@smartgit.com", "@youtube.dk", "@hotmail.com", "@gmail.com", "@yahoo.dk", "@blizzard.com", "@maersk.dk", "@apple.dk" };
         readonly string[] _country = { "Danmark", "Sverige", "Norge", "Tyskland", "Frankrig", "Finland", "Rusland", "Spanien", "Holland" };
         readonly string[] _cities = { "Svenstrup", "Paris", "Oslo", "Odense", "Skive", "Horsens", "København", "Viborg", "Ulstrup", "Göteborg", "Warszawa" };
-        readonly string[] _adressP1 = {"Sved","Kage","Nodre","Faldskærms","Danmarks","Pakke","Ligge","Internet"};
+        readonly string[] _adressP1 = {"Majs","Kage","Nodre","Faldskærms","Danmarks","Pakke","Ligge","Internet"};
         readonly string[] _adressP2 = { "vej", "ly", "krattet", "gade" };
         readonly double[] _dimensions = { 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15, 15.5, 16, 16.5, 17, 17.5, 18, 18.5, 19, 19.5, 20, 20.5, 21, 21.5, 22, 22.5, 23, 23.5, 24, 24.5, 25 };
 
